d69dee7483c2e906ce8997a14b4fbc635b33fcda
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-02-02 17:50:03 +0000

    datalesson completed

---
+++ datatypelesson.py
@@ -0,0 +1,11 @@
+# functions are just instructions that do a job
+
+# take in user data.
+name= input('what is your name')
+age= input ("where do you live")
+location= input("where do you live")
+
+# print/put out data.
+print(name)
+print(age)
+print(location)
