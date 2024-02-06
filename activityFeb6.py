125cc348c0f25c1d410af178a3ef25ed9fd04414
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-02-06 17:55:33 +0000

    dailyassignmentcompletedfeb6

---
+++ activityFeb6.py
@@ -0,0 +1,30 @@
+# Q: What is complexity ? 
+# A: Is the measure of resources a program uses
+# Resources = time, data, energy.
+
+# Q: What is abstraction ?
+# A: The process of simplifiying or hiding complexity.
+
+# Q: What is a variable ?
+# A: A container for data
+
+# data is stored in variables
+# this is an example of extraction
+
+passcode = xc vjbskbjsvdyfuw4t8twyefksdvf
+visa = 4653 3654 3654 3654
+
+# A: variable are a type of extraction
+
+# Rules to naming variables
+# 1: names must be short, and descriptive.
+# EX: visaCard: Id_324234 | Name is short, and descriptive
+
+# 2: variablenames can start with a letters or underscore.
+  _studentID=''
+studentid=''
+
+# 3: variables CANNOT be python keywords.
+
+# Of the following: class, AssertionError, Ect.
+
