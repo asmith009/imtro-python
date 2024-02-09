31b532a67a72e84ea8cbdd136ca812822c849262
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-02-09 17:58:17 +0000

    activityCompletedFeb9

---
+++ activityFeb9.py
@@ -0,0 +1,9 @@
+User_Number1 = int(input('enter a number'))
+User_Number2 = int(input('enter a number'))
+User_Number3 = int(input('enter a number'))
+print(User_Number1 * User_Number2 * User_Number3)
+
+UserName_ = input('UserName')
+UserShoeSize_ = int(input('UserShoeSize'))
+UserAge_ = int(input('UserAge'))
+print(f'Hi my name is {UserName_}, I wear size {UserShoeSize_}, and im {UserAge_}')
---
+++ lessonFeb9.py
@@ -0,0 +1,23 @@
+# prnt()
+# input()
+
+# data casting functions --> 
+
+age= 13
+print(f"hi i am {age} years old")
+
+print('hi i am' + str(age) + 'years old.')
+
+#int()
+#str()
+
+age= 10
+
+# concatenate = means combine
+print('Hi, I am' + str(age) + 'years old.')
+print(f'Hi. I am {age} years old.')
+
+# SyntaxError = an error where the computer doesn't understand the structure of your code.
+
+datatype = "string"
+datatype@= 'eoifnrnvo'
