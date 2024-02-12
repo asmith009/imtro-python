ede66291c6c941dd2ab801193e5c7a0e571daedc
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-02-12 17:08:45 +0000

    dailyassignmentcompletedFeb12

---
+++ warmupFeb12.py
@@ -0,0 +1,21 @@
+# Q1:
+# since it is an string the data will be put out as the same (20)
+# Q2: since the number isnt a name it wont have a function so there is nothiung to be printed out 
+
+# or the function you typed cant function because of incorrectly typed.
+
+# When the computer says syntaxError, you typed something that the computer cant read
+
+# components to a list
+# 1: orederd
+# 2: changeable
+# 3: duplicatable
+
+pen = -0
+marker = 1
+phone = 2
+charger = 3
+BookBag = ['pen', 'marker' , 'phone' , 'charger']
+print (f('pen + marker + phone + charger'))
+
+
