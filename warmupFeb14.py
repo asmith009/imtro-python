686ddaa4e174ebb1292cc8c46cc616ba06489b0e
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-02-14 17:51:08 +0000

    dailyassignmentcompletedFeb14

---
+++ warmupFeb14.py
@@ -0,0 +1,21 @@
+# 1: A built in funtion is a function that the computer already does when a block of code is printed.
+
+# 2: A built in input function is the f, or function. Allows the user to enter data.
+
+# 3: A type of data that can be asisgned in a list of data is integer.
+
+# peanutbuterAndJelly()
+
+def cheeseSteak():
+    print('Ingredients: Steak, Salt, pepper, chopped onion, mayo, kethcup, choice of cheese, hoagie roll ')
+    print('Cookware: spatula, frying pan, stove, oil, knife')
+    print('step 1. turn on the stove to 350 degrees the add oil evenly, and wait 3 minutes til oil is hot ')
+    print('step 2. once oil is hot/ pan is hot, apply steak to pan. season with salt and pepper. fully cook the meat ')
+    print('step 3. Sautee chopped onions until glazed/ goldenbrown.')
+    print('step 4. Add onions to the steak')
+    print('step 5. cut the bread in half, add cheese, mayo, ketchup')
+    print('step 6. Add meat to hogie bread.')
+
+cheeseSteak()
+
+    
