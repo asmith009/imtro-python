18cd87cb2b3ad95b59187799d92c0c534c8589e4
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-02-16 17:05:52 +0000

    amariquizcompleted

---
+++ amariquiz.py
@@ -0,0 +1,93 @@
+#1. What built-in function would you use if you wanted
+# to print out data to a user?
+
+# Answer: I would use the print function.
+
+#2. What built-in function would you use if you wanted 
+# a user to pass in data to a program?
+
+# Answer: I would use the input function.
+
+#3. Explain what complexity and abstraction are, then 
+# provide an example describing their relationship to one another.
+# You cannot use the following examples because they 
+# were shared in class; grocery/ electronics store, car, phone. 
+
+# Answer: complexity is the measure of reasources a program uses.
+# Answer: Abstraction is the process of hiding complexity.
+# Answer: An example would be like a computer mouse. In the unside is a bunch of wiring on the outside its only a couple buttons.
+
+#4. What does it mean when you encounter a syntax error?
+
+# Answer: A syntax error is something the computer cannot read
+
+#5. What is a variable? Provide an example of a variable in code.
+
+# Answer: A variable is a container for data.
+
+#6. Write a function in Python that when called will print out 
+# your name.
+
+# Answer: a function i would use for this is string, and inout, Example: ('enter your name')
+
+#7. What is a function definition and a function call?
+
+# Answer: a function defenition is a a list of code the computer uses to read your functions.
+# Answer: a function call, is typing a function in a block of code so when running your code the computer program will automatically call that function.
+
+#8. Describe what each symbol means?
+
+# = // single equal = assignment
+# == // double equal = comparison
+# != // exclaimation equal = arithmetic
+
+#9. What will the output for the following code block be?
+var1 = 1
+var2 = 2
+var3 = "3"
+
+print(var1 + var2 + var3)
+
+# Answer: syntax error, because the last one has a string attached to it, but not in the print.
+
+
+#10. What will the output for the following code block be?
+listOne = [20, 40, 60, 80]
+listTwo = [20, 40, 60, 80]
+
+print(listOne == listTwo)
+
+# Answer: it would be true, because double equal means comparison , and they are both the same.
+
+# 11. Create the following python lists and include the following
+# data types for each list:
+# a- create a python list of strings. The strings should be items you
+# can find in a book bag.   ListBookbag['pencil', 'notebook', 'calculator']
+# b- Create a python list of floats. the floats should represent the 
+# price of each item in a book bag from the previous list.   ListFloat[1.2, 1.3, 1.4, 1.5]
+
+# 12. What are the Three (3) naming formats we use for naming variables, files, and functions?
+
+# Answer: Letters, Underscores, descriptive.
+
+# 13. Provide a coding scenario where you would you use a comparison operator in a program?
+
+StudentId = 123550
+AmariSmith = 123550
+print(StudentId == AmariSmith)
+
+# 14. Provide a coding scenario Where you would you use a logical operator in a program?
+
+BankAccount = 4,000
+IPhonePrice = 1,500
+print(BankAccount > IPhonePrice)
+
+# 15. Create a block of code where you would 
+# use a comparison operator with the follwing data:
+currentScore= 100
+highScore=2300
+print(currentScore == highScore)
+
+# Bonus : What does the type() function do?
+
+# Answer: I think it returns information.
