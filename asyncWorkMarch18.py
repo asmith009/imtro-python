af49685971c991cfd1caf42b18f9fe523ad7e911
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-03-20 16:53:57 +0000

    activitycompleted

--- asyncWorkMarch18.py
+++ asyncWorkMarch18.py
@@ -1,30 +1,29 @@
 # Async work Monday March 18th, 2024.
 
 #1. Create a function that will count the number of characters in a string
-# that is passed in by a user. The string value can be passed in either as 
+# that is passed in by a user. The string value can be passed in either as
 # a paramter or by using the input function.
 
-#def countLettters():
- #   userCharacters = input('Count Number Of Characters.')
-  #  characters = round(userCharacters)
-   # print(characters)
-#countLettters()
- 
-# You must be write down and explain how your program 
-# works in complete sentences in order to get full credit. 
+    word = input('Type in a string: '):
+    print(len(word))
+def letterCount_Input()
 
-# 1. first i defined the function(countletters).
-# 2. Then I Made a variable for the users input.
+letterCount_Input()
+ 
+# You must be write down and explain how your program
+# works in complete sentences in order to get full credit.
+(countletters).
+# 2. Then I Made a variable for 
+# 1. first i defined the functionthe users input.
 # 3. Then i made a varaible to count the letters in the string
-# 4. Then i used the orint function to print the number of characters.
-# 5. than i called my function.
+# 4. than i called my function.
 
 # 2. Use your notes, W3schools, and what we have learned in class to develop
 # a simple rock, paper, scissors game. Your game should allow the user to enter a letter
 # that will represent the values rock, paper, and scissors (ex. r = rock, p = paper, s= scissors).
 
-def winner(Computer_Move,Player_Move):
-    if Computer_Move == Player_Move:
+def winner(Player_Move, Computer_Move):
+    if Player_Move == Computer_Move:
         print('Tie')
     elif Player_Move == 'Rock' and Computer_Move == 'paper':
         winner = 'computer'
@@ -32,19 +31,17 @@ def winner(Computer_Move,Player_Move):
         winner = 'Computer'
     elif Player_Move == 'Scissor' and Computer_Move == 'Rock':
         winner = 'Computer'
-    elif Player_Move == 'paper' and Computer_Move == 'Rock':
-        winner = 'Player'
     else:
-        print('Computer Win.')
+        print('tie.')
 
-winner('paper','Rock')
+winner('Rock','paper')
 
 
 # EXTREMELY IMPORTANT NOTE
 # at the top of you page write import random
-# use the random.randrange(1,3) function to develop your random value logic 
-# for your rock, paper, scissor game. 
+# use the random.randrange(1,3) function to develop your random value logic
+# for your rock, paper, scissor game.
 
 # Please describe the steps you took, or if you weren't able to complete this activity,
-# the steps you would've taken to solve this problem in complete sentences. 
-# This must be completed in order to get full credit.  
+# the steps you would've taken to solve this problem in complete sentences.
+# This must be completed in order to get full credit.
