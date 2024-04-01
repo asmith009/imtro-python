3a57015d1882c8c4ff33d2099f14bb57091f1b8f
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-04-01 15:17:18 +0000

    asyncWorkApril1completed

---
+++ asyncWorkApril1.py
@@ -0,0 +1,35 @@
+# Async Activity - Monday April 1st, 2024.
+
+# Watch the following videos and answer the following questions. 
+# If necessary, research the questions so that you are able to answer them more effictively.
+
+# When you have completed this assignment, save your work as asyncApril1.py.
+# you must submit your work by 11:59 pm today (April 1, 2024) to recieve credit.  
+
+# Video # 1 - Racial wealth gap
+# https://www.youtube.com/watch?v=Mqrhn8khGLM&t=1s
+
+# Video # 2 - Red lining in America 
+# https://www.youtube.com/watch?v=O5FBJyqfoLM
+
+
+# Questions
+
+#1. Search and find the latest (most recent) household income data 
+# for black households compared to white households and write down what you have found.
+# What is the wealth disparity between the two groups? How does this make you feel?
+
+# Answer 1. The wealth disparity between the two groups were more effective in white communities, and households. Black households mostly found in woman, not having alot of income.
+#           This was due to, job losses, that made black households live in distraught, due to the andemic causing people to lose jobs. Not having a good source of income.
+#           On the other hand, in white households, white communities have a higher population of owned businesses and more things as such. So while in vlack communities
+#           we were struggling to make a living for ourselves, White communities were thriving.
+#           How this makes me feel endulges me with fear along with rage due to the unfair source of society gratitude for black people, and families.
+
+#2.  Research and define in your own words what red lining is?
+
+# Answer 2. Red lining is basically whoever controls credit, judging a book by its cover. They are giving credit based pf how they see people. Mostly black people.
+#           Credit companies have dounf ways to "cut white people some slack", but not us, becasue of our circimstances, and where we live.
+
+#3.  Research and define in your own words what compound interest is?
+
+# Answer 3. compound intrest is intrest you gain off of your own intrest.
