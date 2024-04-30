622350e1f1869218af85a731ab3c816f82950310
asmith009 <158201326+asmith009@users.noreply.github.com>
2024-04-30 16:33:30 +0000

    quizcompleted

---
+++ springQuizNotes.py
@@ -0,0 +1,42 @@
+# what is a computer program?
+# Digital INSTRUCTIONS for computers to execute.
+
+# What is Syntax?
+# Computers language rules.
+
+# example of a python function and its syntax (format rules)
+#def myFunction():
+#    print('here are some instructions...')
+#myFunction()
+
+# example of a Java scripts function and its syntax (format rules)
+#function myFunction(){
+ #   console.log('here are some instructions...')
+#}
+#myFunction()
+
+
+# what are data Types?
+# The fundamental building blocks of a programming language.
+# Think of it as our programming languages alphabet.
+'Name' #string
+10 #integer
+1.00 # float
+True #boolean
+var= [] #list data type
+print(type(1))
+
+def multiply():
+    number = input('enter a number to multiply:  ')
+    result = int(number) *3
+    print(result)
+multiply()
+
+# what are python operators? what do they do?
+# operators perfrom actions on data.
+
+action_1 = 2 / 2 # we are doing division. Division is apart of the arithatic family.
+print(action_1)
+
+action_2 = 'Amari' + 'Smith'
+print(action_2)
